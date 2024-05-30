9ce68adb0e28b23f869ca7051ce12ac6059d8ada
gabs712 <gabs712.g@gmail.com>
2024-05-30 15:54:15 -0300

    Add home html.js and index.html logic

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 import './styles/reset.css'
 import './styles/style.css'
+import renderHome from './scripts/home'
+import {cleanContent} from './scripts/functions'
 
+showHome()
+
+document.querySelector('[data-home]').addEventListener('click', showHome)
+document.querySelector('[data-menu]').addEventListener('click', showMenu)
+document.querySelector('[data-contact]').addEventListener('click', showContact)
+
+
+function showHome() {
+cleanContent()
+renderHome()
+}
+
+function showMenu() {
+  cleanContent()
+  showMenu()
+}
+
+function showContact() {
+  cleanContent()
+  renderContact()
+}
