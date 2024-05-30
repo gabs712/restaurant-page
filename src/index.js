5f09b44036af13d3aac11b276b2c90f6fbdc3837
gabs712 <gabs712.g@gmail.com>
2024-05-30 19:46:40 -0300

    Setup contact file and add background color to images

--- src/index.js
+++ src/index.js
@@ -6,24 +6,24 @@ import renderMenu from './scripts/menu.js'
 // import renderContact from './scripts/contact.js'
 import {cleanContent} from './scripts/functions.js'
 
-showHome()
+// showMenu()
 
-document.querySelector('[data-home]').addEventListener('click', showHome)
-document.querySelector('[data-menu]').addEventListener('click', showMenu)
-// document.querySelector('[data-contact]').addEventListener('click', showContact)
+// document.querySelector('[data-home]').addEventListener('click', showHome)
+// document.querySelector('[data-menu]').addEventListener('click', showMenu)
+// // document.querySelector('[data-contact]').addEventListener('click', showContact)
 
 
-function showHome() {
-cleanContent()
-renderHome()
-}
-
-function showMenu() {
-  cleanContent()
-  renderMenu()
-}
-
-// function showContact() {
-//   cleanContent()
-//   renderContact()
+// function showHome() {
+// cleanContent()
+// renderHome()
 // }
+
+// function showMenu() {
+//   cleanContent()
+//   renderMenu()
+// }
+
+// // function showContact() {
+// //   cleanContent()
+// //   renderContact()
+// // }
